87d4af36dde504303b53213a4b4165cda17246ea
std67122420325-cloud <std.67122420325@ubru.ac.th>
2026-01-23 11:15:24 +0700

    frint commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Week09
+# Chapter06
+## Flask &SQLAlchemy
+* บทเรียนรู้การใช่งาน Flask รับกัน SQLAlchemy 
---
+++ epl/models.py
@@ -0,0 +1,29 @@
+from epl import  db
+from sqlalchemy import Integer,String,ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column,relationship
+from typing import List
+
+class Club(db.Model):
+  __tablename__ = 'club'
+  id: Mapped[int] = mapped_column(Integer,primary_key=True)
+  name: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
+  stadium: Mapped [str] = mapped_column(String(50), nullable=False)
+  year: Mapped[int] = mapped_column(Integer, nullable=False)
+  logo: Mapped[str] = mapped_column(String(256),nullable=False)
+
+  players: Mapped[List['Player']] = relationship(back_populates='club')
+  def __repr__(self):
+    return f'<Club : {self.name}>'
+
+class Player(db.Model):
+  __tablename__ = 'player'
+  id: Mapped[int] = mapped_column(Integer,primary_key=True)
+  name: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
+  position: Mapped [str] = mapped_column(String(50), nullable=False)
+  nationality: Mapped [str] = mapped_column(String(50), nullable=False)
+  img: Mapped[str] = mapped_column(String(256),nullable=False)
+  clud_id: Mapped[int] = mapped_column(Integer,ForeignKey(Club.id))
+  
+  club: Mapped[Club] =relationship(back_populates='players')
+  def __repr__(self):
+    return f'<Player : {self.name}>'
